f5e4a62a83686405a31a353569fd7eed7e3d0746
Aadhav Senthil <aadhavs0325@gmail.com>
2024-03-11 18:32:49 -0500

    worked on user-login and edit and new item

--- daintree/root/urls.py
+++ daintree/root/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm
 
 
 app_name = 'root'
@@ -8,5 +9,6 @@ app_name = 'root'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
-    
+    path('login/', auth_views.LoginView.as_view(template_name='root/login.html',authentication_form=LoginForm), name='login')
+
 ]
