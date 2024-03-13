39d6d98ece0aff1ad09c2c49f3f3bbd74dfd333e
Gabriel Asare <gabrielasare@Gabriels-MacBook-Pro.local>
2024-03-12 20:00:02 -0500

    fixed errors in editing items and deleting items

--- daintree/item/views.py
+++ daintree/item/views.py
@@ -70,4 +70,4 @@ def delete(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
 
-    return redirect('dashboard: index')
+    return redirect('dashboard:index')
