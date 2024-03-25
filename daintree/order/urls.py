3b94eb8d40d8bad535a6206835796e52fea42a2b
Gabriel Asare <gabrielasare@Gabriels-MacBook-Pro.local>
2024-03-25 10:49:27 -0500

    added a quantiy change feature, and notification to sellers when their item gets purchased

--- daintree/order/urls.py
+++ daintree/order/urls.py
@@ -7,6 +7,7 @@ app_name = 'order'
 urlpatterns = [
     path('<int:pk>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('<int:pk>/remove_from_cart/', views.remove_from_cart, name='remove_from_cart'),
+    path('<int:pk>/update_item_quantity/', views.update_item_quantity, name='update_item_quantity'),
     path('cart/', views.view_cart, name='view_cart'),
     path('checkout/', views.checkout, name='checkout'),
 ]
